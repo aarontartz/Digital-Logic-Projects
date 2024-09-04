043a7ece9a20fc16db3875243264a8f7a790e665
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2024-09-04 15:01:00 -0700

    Create bcd.v

---
+++ Seg7Disp/bcd.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/04/2024 02:27:04 PM
+// Design Name: 
+// Module Name: bcd
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bcd(
+    input wire [3:0] d,
+    input wire clk,
+    input wire en,
+    input wire load,
+    input wire up,
+    input wire clr,
+    output wire [3:0] q,
+    output reg co
+    );
+    
+    reg [3:0] count;
+    assign q = count;
+    
+    always @(posedge clk or negedge clr) begin
+        if (clr == 0) begin
+            count <= 4'd0;
+            co <= 1'b0;
+        end
+        else if (en == 1) begin
+            if (load == 1) begin
+                count <= d;
+                co <= 0; // good practice
+            end
+            else if (load == 0) begin
+                if (up == 1) begin
+                    if (count == 9) begin
+                        count <= 4'd0;
+                        co <= 1;
+                    end
+                    else begin
+                        count <= count + 1;
+                        co <= 0; // remember to set to 0
+                    end
+                end
+                else if (up == 0) begin
+                    if (count == 0) begin
+                        count <= 4'd9;
+                        co <= 1;
+                    end
+                    else begin
+                        count <= count - 1;
+                        co <= 0; // remember to set to 0
+                    end
+                end
+            end
+        end
+    end
+endmodule
