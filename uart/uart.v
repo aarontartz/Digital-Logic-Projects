3a85f3c681258923518f35cd901da52fe6c78154
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2024-09-24 11:30:31 -0700

    Update uart.v

--- uart/uart.v
+++ uart/uart.v
@@ -23,16 +23,40 @@
 module uart(
     input wire baud_clk,
     input wire [7:0] data_in,
-    output wire o_bit
+    output reg o_bit
     );
     
     localparam [3:0] START = 2'b00,
                      DATA  = 2'b01,         
-                     END   = 2'b10,
+                     STOP  = 2'b10,
                      IDLE  = 2'b11;
                      
-    reg [1:0] state = START;    // state register
+    reg [1:0] state = IDLE;    // state register
+    reg [7:0] data;
+    reg [3:0] index = 0;
     
+    always @(posedge baud_clk) begin
+        case (state)
+            START: begin
+                data <= data_in;
+                o_bit <= 0;
+            end
+            DATA: begin
+                o_bit <= data[index];
+            end
+            STOP: begin
+                o_bit <= 1;
+                index <= 0;
+            end
+            IDLE: begin
+                o_bit <= 1;
+            end
+        endcase
+    end
     
+    always @* begin
+        if ((state == DATA) && (index > 0))
+            index = index + 1;
+    end
     
 endmodule
