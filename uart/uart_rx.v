15d5389830175a241396ff88a0eba282d15dae17
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2024-10-09 02:34:59 -0700

    Update uart_rx.v

--- uart/uart_rx.v
+++ uart/uart_rx.v
@@ -2,18 +2,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Engineer: Aaron Tartz
 // 
-// Create Date: 09/24/2024 09:44:13 AM 
+// Create Date: 09/24/2024 07:23:04 PM 
 // Module Name: uart_rx
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module uart_rx(
-    input wire baud_clk,
-    input wire data_en,
-    input wire data_in,
-  output reg [7:0] data_out,
-    output wire fsm_clk
+    input wire rx_pulse,
+    input wire rx_en,
+    input wire rx_in,
+    output reg [7:0] data_out
     );
     
     localparam [1:0] START = 2'b00,
@@ -22,94 +21,44 @@ module uart_rx(
                      IDLE  = 2'b11;
                      
     reg [1:0] state = IDLE;    // state register
-    reg [7:0] data;
+    
+    reg rx_in_reg;
+    reg [7:0] data = 0;
     reg [3:0] index = 0;
     
-    reg fsm_clk_reg = 0;
-    assign fsm_clk = fsm_clk_reg;
-
-
-    always @(posedge baud_clk) begin
+    always @(posedge rx_pulse) begin
+        rx_in_reg <= rx_in;
         case (state)
             START: begin
                 state <= DATA;
             end
             DATA: begin
-                if (index == 7)
+                if (index == 7 && rx_in == 1)
                     state <= STOP;
                 else
                     index <= index + 1;
             end
             STOP: begin
                 index <= 0;
-                if (data_en == 1)
+                if (rx_en == 1)
                     state <= START;
                 else
                     state <= IDLE;
             end
             IDLE: begin
+                if (rx_en == 1 && rx_in == 0)
+                    state <= START;
             end
         endcase
     end
 
     always @(*) begin
         case (state)
-            START: begin
-                data = 0;
-            end
             DATA: begin
+                data[index] = rx_in_reg;
+            end
+            STOP: begin
                 data_out = data;
-            end
-            STOP: begin
-            end
-            IDLE: begin
-            end
-        endcase
-    end
-
-    
-    
-    always @(posedge baud_clk) begin
-        case (state)
-            START: begin
-                fsm_clk_reg <= 1;
-                data <= data_in;
-                state <= DATA;
-            end
-            DATA: begin
-                if (index == 7)
-                    state <= STOP;
-                else
-                    index <= index + 1;
-            end
-            STOP: begin
-                index <= 0;
-                fsm_clk_reg <= 0;
-                if (data_en == 1)
-                    state <= START;
-                else
-                    state <= IDLE;
-            end
-            IDLE: begin
-                if (data_en == 1)
-                    state <= START;
-            end
-        endcase
-    end
-    
-    always @(*) begin
-        case (state)
-            START: begin
-                o_bit = 0;
-            end
-            DATA: begin
-                o_bit = data[index];
-            end
-            STOP: begin
-                o_bit = 1;
-            end
-            IDLE: begin
-                o_bit = 1;
             end
         endcase
     end
