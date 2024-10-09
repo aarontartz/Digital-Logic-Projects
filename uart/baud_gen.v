3b15ece00140c529e78ca9239e13f43c8b2f88ea
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2024-10-09 02:33:58 -0700

    Update baud_gen.v

--- uart/baud_gen.v
+++ uart/baud_gen.v
@@ -10,19 +10,27 @@
 
 module baud_gen(
     input wire clk,
-    output wire baud_clk
+    output wire baud_clk,
+    output wire rx_pulse
     );
     
-    localparam [15:0] BAUD_SCALE = 16'd100;  // just arbitrary value, can change later for exact baud rate
+    // 9600 = 100MHz / (10 * x)
+    // x = 1302
+    
+    localparam [7:0] BAUD_SCALE = 8'd100;
     
     reg [31:0] count = 0;
-    reg baud_clk_reg = 0;
+    reg baud_reg = 0;
+    reg rx_pulse_reg = 1;
     
-    assign baud_clk = baud_clk_reg;
+    assign baud_clk = baud_reg;
+    assign rx_pulse = rx_pulse_reg;
     
     always @(posedge clk) begin
+        if ((count == 2 && baud_reg == 0) || (count == 98  && baud_reg == 1 ))
+            rx_pulse_reg <= ~rx_pulse_reg;
         if (count == BAUD_SCALE) begin
-            baud_clk_reg <= ~baud_clk_reg;
+            baud_reg <= ~baud_reg;
             count <= 0;
         end
         else
